797535d1362df61100e0b3f8bbc60dd7179a1366
nadinaaa <43186448+nadinaaa@users.noreply.github.com>
2018-10-07 18:51:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-//
 module.exports = function getZerosCount(number, base) {
-  let  div = 2,masp=[],mask=[],i=0,k=0;
-  while (base > 1)
-  {
-    if(base % div == 0)
-    {
-      k=0;
-      masp[i]=div;
-      while (base % div == 0)
-      {
-        k++;
-        mask[i]=k;
-        base = base / div;
+  let ze=number;
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let k = 0;
+      while (base % i === 0) {
+         base /= i;     
+         k++; 
       }
-      i++;
-     }
-     if (div == 2) div++;
-     else div += 2;
-  }
-  let L=masp.length,sa=[],tmp;
-  for(i =0; i<L;i++)
-    sa[i]=0;
-  for (i = 2;i <=number;i++)
-  {
-   tmp=i;
-   for (j=0;j<L;j++) 
-   {
-    while (tmp%masp[j]==0) 
-    {
-      tmp/=masp[j]; 
-      sa[j]++;
+      let z = Math.floor(number / i),a=0;
+      while (z > 0) {
+        a += z;
+        z = Math.floor(z / i);
+      }
+      ze = Math.min(ze, Math.floor(a / k))
     }
-   }
-  } 
-  tmp=sa[0]/mask[0]
-  for (i = 1; i<L;i++) 
-    tmp = Math.min(tmp, sa[i]/mask[i]);
-  tmp=Math.trunc(tmp);
-  return(tmp);
+  }
+  return ze;
 }
